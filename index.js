563598b59a008f3934652b3c0f4bb70faa481937
Nicholas Amideo <namideo@Nicholass-MacBook-Pro.local>
2022-03-28 22:46:44 -0400

    index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
+const team = [];
+
+
+
 const addManager = () => {
     return inquirer.prompt([
 
@@ -30,6 +34,13 @@ const addManager = () => {
             message: 'Office Number'
         },
     ])
+
+    .then(response => {
+        let manager = new Manager(response.name, response.ID, response.email, response.officeNumber);
+        team.push(manager)
+        addEmployee()
+    })
+
 };
 
     const addEmployee = () => {
@@ -38,45 +49,75 @@ const addManager = () => {
         {
             type: 'list',
             name: 'employee',
-            message: 'Select an employee position',
-            choices: ['Engineer', 'Intern'] 
+            message: 'Select employee position',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee name.'
         },
         {
             type: 'input',
             name: 'ID',
-            message: 'Input employee Id'
+            message: 'Input Id'
         },
         {   
             type: 'input',
             name: 'email',
-            message: 'Input employee email address'
+            message: 'Input email address'
         },
         {
             type: 'input',
             name: 'github',
             message: 'GitHub username?',
-            when: (input) => input.employee = "Engineer"          
+            when: (input) => (input.employee) === "Engineer"      
         },
         {
             type: 'input',
             name: 'school',
-            message: 'What school are you currently enrolled in?', 
-            when: (input) => input.employee = "Intern"
-         },
-         {
+            message: 'What school are you currently enrolled in?',
+            when: (input) => (input.employee) === "Intern"
+        },
+        {
             type: 'list',
             name: 'newEmployee',
-            message: 'Add employee?',
-            choices: ['yes', 'no']
-         },
+            message: "Add another employee?",
+            choices: ["yes", "no"]
+        }
     ])
-};
 
-function init() {
-    addManager()
-    .then((response) => {fs.writeFile("generateHTML.js", generateHTML(response))
-    .catch((err) => console.error(err));
-    }
-    )}
+        .then(function(response) {
+            let newMember;
+            if (response.employee === "Engineer") {
+                newMember = new Engineer(response.name, response.ID, response.email, response.github);
+            } else if (response.employee === "Intern") {
+                newMember = new Intern(response.name, response.ID, response.email, response.school);
+            }
+            team.push(newMember);
 
-init();
+                if (response.newEmployee === "yes") {
+                    return addEmployee(team);
+                } else {
+                    return team;
+                }
+            })
+    };
+
+    function init() {
+        addManager()   
+
+            .then(() => {
+                fs.writeFileSync("output.html", generateHTML("dist/index.html", team))
+            }) 
+            
+            .catch((error) => {
+                console.log(error)
+            })
+    
+        .catch((error) => { console.log(error)});
+        
+        };
+    
+
+    init();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,46 +1,84 @@
+const fs = require('fs');
+
 function generateManager(manager) {
     return `
-    <h3></h3>
+        <div class = "card col-6">
+            <div class = card-header>Manager</div>
+            <p>Manager Name: ${manager.name}</p>
+            <p>ID: ${manager.ID}</p>
+            <p>Email address:${manager.email}</p>
+            <p>Office Number:${manager.officeNumber}</p>
+        </div>
+        `;
+}
+
+function generateEngineer(employee) {
+    return ` 
+    <div class = "card col-6">
+        <div class = "card-header">Engineer</div>
+            <p>Name: ${engineer.name}</p>
+            <p>ID: ${engineer.ID}</p>
+            <p>Email address: ${engineer.email}</p>
+            <p>GitHub: ${engineer.github}</p>
+        </div>
+        `;
+}
+
+function generateIntern(intern) {
+    return `
+    <div class = "card col-6">
+        <div class = "card-header">Intern</div>
+        <p>Name: ${intern.name}</p>
+        <p>ID: ${intern.ID}</p>
+        <p>Email address: ${intern.email}</p>
+        <p>School: ${intern.school}</p>
+    </div>
     `;
 }
 
 
 
-function generateHTML(manager) {
+function generateHTML() {
     return `
-    ${manager.name}
-
-`;
-
-`<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Team Profile</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-            integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"/>
-
-  <link rel="stylesheet" href="assets/style.css"/>
-</head>
-
-<body>
-    <header>
-        <nav class="navbar" id="navbar">
-            <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
-        </nav>
-    </header>
-
-    <div class = "container">
-        <div class = "row, col-6">
-        </div>
-
-    </div>
+    <!DOCTYPE html>
+    <html lang="en">
     
-</body>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+                integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"/>
+    
+      <link rel="stylesheet" href="style.css"/>
+    </head>
+    
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+    
+        <div class = "container">
+            <div class = "row">
 
-</html>
-`};
+
+
+            </div>
+                   
+                
+            </div>
+    
+        </div>
+        
+    
+    </body>
+    
+    </html>
+    `;
+
+
+}
 
 module.exports = generateHTML;
